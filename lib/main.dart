6053420a237222ae72f7f174c334b69bd5e87204
ZIN MYO <overmidnight12@gmail.com>
2020-05-16 13:04:31 -0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:flutterfooddelivery/pages/StarterPage.dart';
+
+
+void main() => runApp(
+  MaterialApp(
+    theme: ThemeData(),
+    home:  StarterPage(),
+  )
+);
---
+++ lib/pages/StarterPage.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class StarterPage extends StatefulWidget {
+  @override
+  _StarterPageState createState() => _StarterPageState();
+}
+
+class _StarterPageState extends State<StarterPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/images/kfc.png'),
+          )
+        ),
+      ),
+    );
+  }
+}
+
+
